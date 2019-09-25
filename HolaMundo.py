718539613a8225d20d0bcb89cb184efa7f7c614c
Estefania Esquivel Camacho <55338388+Estefaniaaaa@users.noreply.github.com>
2019-09-25 01:05:46 -0500

    Create HolaMundo.py

---
+++ HolaMundo.py
@@ -0,0 +1,10 @@
+# HolaMundo.py
+# Autor: Eilen Estefania Esquivel Camacho
+# Fecha de creación: 16/09/2019
+
+# La función print es clave para la salida de datos. 
+
+# Manda cualquier cosa que pongamos entre las comillas (o paréntesis) 
+# que siguen a print a la salida.
+
+print("Hola Mundo, ahora en Python!")
